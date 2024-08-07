cb3441ec6d71f4ea4c9c9bd86fa6ab3712c6d24f
Davasam Karthikeya <davasamkarthikeya@gmail.com>
2024-08-07 10:22:25 +0530

    Complete Week3

--- Week3/q4.java
+++ Week3/q4.java
@@ -15,21 +15,21 @@ class Stack{
 	int stack[],tos,size;
 	Stack(int size){
 	    this.size = size;
-	    tos = -1
+	    this.tos = -1;
 	    this.stack = new int[size];
 	}
 	boolean is_full(){
-		return this.tos == this.size - 1
+		return this.tos == this.size - 1;
 	}
 	boolean is_empty(){
-		return self.tos == -1
+		return this.tos == -1;
 	}
 	void push(int value){
 		if(this.is_full()){
 			System.out.println("Stack overflow! Cannot push new element");
 			return;
 		}
-		this.tos += 1
+		this.tos += 1;
 		this.stack[this.tos] = value;
 	}
 	int pop(){
@@ -38,12 +38,26 @@ class Stack{
 			return -1;
 		}
 		int value = this.stack[this.tos];
+		tos--;
+		return value;
 	}
-
+	void display(){
+		for(int i = this.tos;i>=0;i--){
+			if(i == 0)System.out.print(stack[i]);
+			else System.out.print(stack[i]+",");
+		}
+		System.out.println();
+	}
 }
 
 class q4{
 	public static void main(String args[]){
-		System.out.println("Hello World!");
+		Stack stack = new Stack(3);
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		stack.display();
+		int value = stack.pop();
+		stack.display();
 	}
 }
