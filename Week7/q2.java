1849a8a450e787d91873b74fe0aa6021bf404387
Davasam Karthikeya <davasamkarthikeya@gmail.com>
2024-09-30 17:00:01 +0530

    Complete Week7

--- Week7/q2.java
+++ Week7/q2.java
@@ -15,5 +15,9 @@ class q2{
 	public static void main(String args[]){
 		int Arr[][] = {{1,2,3}};
 		System.out.println(Maximum.max(Arr));
+		int Arr1[] = {1,2,3};
+		System.out.println(Maximum.max(Arr1));
+		System.out.println(Maximum.max(1,2,3));
+		System.out.println(Maximum.max((float)0.1,(float)0.2,(float)0.3));
 	}
 }
